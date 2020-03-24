fa25394a83d34c13024353dc82a26159e6cbba3a
tosswi <Heckel3437@gmail.com>
2020-03-24 14:51:23 +0900

    6章

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,5 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-     get root_path
-    assert_response :success
-  end
-
-  test "should get help" do
-    get help_path
-    assert_response :success
-  end
-  test "should get about" do
-    get static_pages_help_url
-    assert_response :success
-  end
-     test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-  end
 
 end
